eb20f7c158abfd261a78057e4bb2ba825be5e30a
iamtiedev <facundotiede@gmail.com>
2023-06-12 16:50:20 -0300

    refactor code

--- src/com/classSeven/FiveNumbers.java
+++ src/com/classSeven/FiveNumbers.java
@@ -12,36 +12,72 @@ import java.util.Scanner;
 
 public class FiveNumbers {
 
-    public static void main(String[] args) {
+    private static final int NUM_ELEMENTS = 5;
 
+    public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
 
-        int fiveNumbers[] = new int[5];
-        int major = 0;
-        int minor = 99999;
-        double prom = 0;
+        int[] numbers = new int[NUM_ELEMENTS];
+        double sum = 0;
 
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Ingrese un numero");
-            fiveNumbers[i] = console.nextInt();
-            prom += fiveNumbers[i];
-            if (fiveNumbers[i] > major) {
-                major = fiveNumbers[i];
-            }
-            if (fiveNumbers[i] < minor) {
-                minor = fiveNumbers[i];
-            }
+        for (int i = 0; i < NUM_ELEMENTS; i++) {
+            System.out.println("Ingrese un numero para la posicion " + (i + 1) + ":");
+            numbers[i] = readValidNumber(console);
+            sum += numbers[i];
         }
+
         System.out.println("Los numeros son: ");
-        for (int numbers : fiveNumbers) {
-            System.out.println(numbers);
-        }
+        printNumbers(numbers);
+
+        int major = findMajorNumber(numbers);
+        int minor = findMinorNumber(numbers);
+        double prom = calculateAverage(sum);
 
         System.out.println("Numero mayor: " + major);
         System.out.println("Numero menor: " + minor);
-        System.out.println("Promedio: " + prom / fiveNumbers.length);
+        System.out.println("Promedio: " + prom);
 
     }
 
+    private static int readValidNumber(Scanner console) {
+        while (true) {
+            if (console.hasNextInt()) {
+                return console.nextInt();
+            } else {
+                System.out.println("Entrada invalida! Favor de ingresar numero valido.");
+                console.next(); // Clean buffer
+            }
+        }
+    }
+
+    private static void printNumbers(int[] fiveNumbers) {
+        for (int number : fiveNumbers) {
+            System.out.println(number);
+        }
+    }
+
+    private static int findMajorNumber(int[] fiveNumbers) {
+        int largest = Integer.MIN_VALUE;
+        for (int number : fiveNumbers) {
+            if (number > largest) {
+                largest = number;
+            }
+        }
+        return largest;
+    }
+
+    private static int findMinorNumber(int[] fiveNumbers) {
+        int smallest = Integer.MAX_VALUE;
+        for (int number : fiveNumbers) {
+            if (number < smallest) {
+                smallest = number;
+            }
+        }
+        return smallest;
+    }
+
+    private static double calculateAverage(double sum) {
+        return sum / NUM_ELEMENTS;
+    }
 
 }
