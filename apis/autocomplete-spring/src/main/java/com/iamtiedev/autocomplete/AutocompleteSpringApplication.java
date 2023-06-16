e2ed6bb425705543f9ca8ffe33bc6a20495973ae
iamtiedev <facundotiede@gmail.com>
2023-06-15 23:46:10 -0300

    update path

--- apis/autocomplete-spring/src/main/java/com/iamtiedev/autocomplete/AutocompleteSpringApplication.java
+++ apis/autocomplete-spring/src/main/java/com/iamtiedev/autocomplete/AutocompleteSpringApplication.java
@@ -1,5 +1,11 @@
 package com.iamtiedev.autocomplete;
 
+/*
+“Estamos armando el servicio de autocompletar de Google.
+Tienes que desarrollar una aplicación en springboot que recibe lo que está escribiendo el usuario, y devuelve un listado de posibles resultados.
+Para hacer esto en un array de String carga resultados de ejemplo”.
+ */
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
