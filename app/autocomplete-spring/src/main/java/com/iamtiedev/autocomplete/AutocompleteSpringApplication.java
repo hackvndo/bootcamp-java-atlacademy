671dd19b8b4a480d54b39e5bc90f39fe87442428
iamtiedev <facundotiede@gmail.com>
2023-06-15 21:49:17 -0300

    change path folder

---
+++ app/autocomplete-spring/src/main/java/com/iamtiedev/autocomplete/AutocompleteSpringApplication.java
@@ -0,0 +1,13 @@
+package com.iamtiedev.autocomplete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocompleteSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutocompleteSpringApplication.class, args);
+	}
+
+}
---
+++ app/autocomplete-spring/src/test/java/com/iamtiedev/autocomplete/AutocompleteSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamtiedev.autocomplete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutocompleteSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
