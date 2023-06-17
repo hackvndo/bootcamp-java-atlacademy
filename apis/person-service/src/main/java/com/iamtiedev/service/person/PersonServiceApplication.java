fc6a2e8963e88469bb352f73b441f414f715b2a7
iamtiedev <facundotiede@gmail.com>
2023-06-16 23:25:31 -0300

    update api services finished

--- apis/person-service/src/main/java/com/iamtiedev/service/person/PersonServiceApplication.java
+++ apis/person-service/src/main/java/com/iamtiedev/service/person/PersonServiceApplication.java
@@ -1,5 +1,10 @@
 package com.iamtiedev.service.person;
 
+/*
+“Crea un servicio web en springboot que tenga un arreglo con nombres completos de personas.
+Debe permitir realizar una búsqueda por el nombre de la persona, agregar personas y también borrarlas”. 
+ */
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- apis/person-service/src/main/java/com/iamtiedev/service/person/controller/PersonController.java
+++ apis/person-service/src/main/java/com/iamtiedev/service/person/controller/PersonController.java
@@ -1,27 +1,17 @@
 package com.iamtiedev.service.person.controller;
 
 import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/persons")
 public class PersonController {
 
-    private List<String> persons = new ArrayList<>();
-
-    public PersonController() {
-        persons.add("John Doe");
-        persons.add("Jane Smith");
-        persons.add("Michael Johnson");
-        persons.add("Emily Davis");
-        persons.add("David Brown");
-        persons.add("Olivia Taylor");
-        persons.add("William Miller");
-        persons.add("Sophia");
-        persons.add("Joseph Anderson");
-        persons.add("Mia Martinez");
-    }
+    static List<String> persons = new ArrayList<>(List.of("Facundo Tiede", "John Doe", "Jane Smith", "Michael Johnson",
+            "Emily Davis"));
 
     @GetMapping("/search")
     public List<String> getAllPersons() {
@@ -30,27 +20,21 @@ public class PersonController {
 
     @GetMapping("/search/{name}")
     public List<String> searchPersonByName(@PathVariable String name) {
-        List<String> matchingPersons = new ArrayList<>();
-        for (String person : persons) {
-            if (person.toLowerCase().contains(name.toLowerCase())) {
-                matchingPersons.add(person);
-            }
-        }
-        return matchingPersons;
+        return persons.stream()
+                .filter(person -> person.toLowerCase().contains(name.toLowerCase()))
+                .collect(Collectors.toList());
     }
 
-    @PostMapping("/update/{name}")
-    public void addPerson(@PathVariable String name) {
+    @GetMapping("/add/{name}")
+    public List<String> addPerson(@PathVariable String name) {
         persons.add(name);
+        return persons;
     }
 
-    @DeleteMapping("/delete/{name}")
-    public void deletePerson(@PathVariable String name) {
-        for (String person : persons) {
-            if (person.equalsIgnoreCase(name)) {
-                persons.remove(person);
-                break;
-            }
-        }
+    @GetMapping("/delete/{name}")
+    public String deletePerson(@PathVariable String name) {
+        persons.removeIf(person -> person.equalsIgnoreCase(name));
+        return "Deleted Name";
     }
+
 }
