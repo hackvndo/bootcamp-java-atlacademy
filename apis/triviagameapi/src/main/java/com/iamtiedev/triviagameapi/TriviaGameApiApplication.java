ba8597e51158cf0ca7dc3967cf25da457bd9bb6a
iamtiedev <facundotiede@gmail.com>
2023-06-20 18:35:53 -0300

    update trivia api

---
+++ apis/triviagameapi/src/main/java/com/iamtiedev/triviagameapi/TriviaGameApiApplication.java
@@ -0,0 +1,13 @@
+package com.iamtiedev.triviagameapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriviaGameApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriviaGameApiApplication.class, args);
+	}
+
+}
